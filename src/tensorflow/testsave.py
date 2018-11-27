8b1b383da2b626787a668827c933a5174b6ae708
chenhao <bing1zhi2@163.com>
2018-11-27 13:25:52 +0800

    add testsave.py test saver 测试不同情况下的saver

--- src/tensorflow/testsave.py
+++ src/tensorflow/testsave.py
@@ -1,14 +1,115 @@
 import tensorflow as tf
-from tensorflow.python import pywrap_tensorflow
-import os
+import numpy as np
+import matplotlib.pyplot as plt
 
-model_dir='F:/dataset/facenet/model/20180408-102900'
+plotdata = {"batchsize": [], "loss": []}
+diffclass = False
 
-checkpoint_path = os.path.join(model_dir, "model-20180402-114759.ckpt-275")
-print(checkpoint_path)
-reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
-var_to_shape_map = reader.get_variable_to_shape_map()
-for key in var_to_shape_map:
-    print("tensor_name: ", key)
-    print(reader.get_tensor(key))
 
+def moving_average(a, w=10):
+    if len(a) < w:
+        return a[:]
+    return [val if idx < w else sum(a[(idx - w):idx]) / w for idx, val in enumerate(a)]
+
+
+# 生成模拟数据
+train_X = np.linspace(-1, 1, 100)
+train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.3  # y=2x，但是加入了噪声
+# 图形显示
+plt.plot(train_X, train_Y, 'ro', label='Original data')
+plt.legend()
+plt.show()
+
+tf.reset_default_graph()
+
+# 创建模型
+# 占位符
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+# 模型参数
+W = tf.Variable(tf.random_normal([1]), name="weight")
+b = tf.Variable(tf.zeros([1]), name="bias")
+# 前向结构
+z = tf.multiply(X, W) + b
+
+# 反向优化
+cost = tf.reduce_mean(tf.square(Y - z))
+learning_rate = 0.01
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)  # Gradient descent
+
+# 初始化变量
+init = tf.global_variables_initializer()
+# 参数设置
+training_epochs = 50
+display_step = 2
+
+tf_var = tf.trainable_variables()
+print(tf_var)
+
+var_to_restore2 = [v for v in tf_var if not v.name.startswith('weight')]
+print('var_to_restore2', var_to_restore2)
+saver_min = tf.train.Saver(var_to_restore2)  # 生成saver
+saver = tf.train.Saver(tf_var)  # 生成saver
+
+# saver = tf.train.Saver()  # 生成saver
+savedir = "log/"
+#启动session
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (x, y) in zip(train_X, train_Y):
+            sess.run(optimizer, feed_dict={X: x, Y: y})
+
+        # 显示训练中的详细信息
+        if epoch % display_step == 0:
+            loss = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", epoch + 1, "cost=", loss, "W=", sess.run(W), "b=", sess.run(b))
+            if not (loss == "NA"):
+                plotdata["batchsize"].append(epoch)
+                plotdata["loss"].append(loss)
+
+    print(" Finished!")
+    saver.save(sess, savedir + "linermodel.cpkt")
+    print("cost=", sess.run(cost, feed_dict={X: train_X, Y: train_Y}), "W=", sess.run(W), "b=", sess.run(b))
+    # print ("cost:",cost.eval({X: train_X, Y: train_Y}))
+
+    # 图形显示
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
+
+    plotdata["avgloss"] = moving_average(plotdata["loss"])
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(plotdata["batchsize"], plotdata["avgloss"], 'b--')
+    plt.xlabel('Minibatch number')
+    plt.ylabel('Loss')
+    plt.title('Minibatch run vs. Training loss')
+
+    plt.show()
+
+
+with tf.Session() as sess2:
+
+
+    sess2.run(tf.global_variables_initializer())
+    if diffclass:
+        saver_min.restore(sess2, savedir + "linermodel.cpkt")
+    else:
+        saver.restore(sess2, savedir + "linermodel.cpkt")
+    print("w，b=", sess2.run(W), sess2.run(b))
+    print ("x=0.2，z=", sess2.run(z, feed_dict={X: 0.2}))
+
+    saver.save(sess2, savedir + "linermodel2.cpkt")
+
+
+# with tf.Session() as sess3:
+#     sess3.run(tf.global_variables_initializer())
+#     saver.restore(sess3, savedir+"linermodel.cpkt")
+#     print("w，b=", sess3.run(W), sess3.run(b))
+#     print ("x=0.2，z=", sess3.run(z, feed_dict={X: 0.2}))
+#
+#     saver_full.save(sess3, savedir + "linermodel3.cpkt")

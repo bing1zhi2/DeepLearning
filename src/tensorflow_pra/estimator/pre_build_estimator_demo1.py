d84e1a55b4576e770cbd00d83c59651e422ddffc
chenhao <bing1zhi2@163.com>
2019-03-05 18:44:34 +0800

    tensorflow models esimator

---
+++ src/tensorflow_pra/estimator/pre_build_estimator_demo1.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    return dataset.shuffle(1000).repeat().batch(batch_size)
+
+

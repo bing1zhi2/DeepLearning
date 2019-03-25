3176c9dc6ed1bb5e5d988b480e44d8549e4ec386
chenhao <bing1zhi2@163.com>
2019-03-25 18:12:04 +0800

    estimator practice

--- src/tensorflow_pra/estimator/pre_build_estimator_demo1.py
+++ src/tensorflow_pra/estimator/pre_build_estimator_demo1.py
@@ -1,12 +1,69 @@
+"""An Example of a DNNClassifier for the Iris dataset."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
 import tensorflow as tf
 
-
-def train_input_fn(features, labels, batch_size):
-    """An input function for training"""
-    # Convert the inputs to a Dataset.
-    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
-
-    # Shuffle, repeat, and batch the examples.
-    return dataset.shuffle(1000).repeat().batch(batch_size)
+import tensorflow_pra.estimator.iris_data as iris_data
 
 
+def main():
+    batch_size = 50
+    train_steps = 5000
+
+    # Fetch the data
+    (train_x, train_y), (test_x, test_y) = iris_data.load_data()
+
+    # Feature columns describe how to use the input.
+    my_feature_columns = []
+    for key in train_x.keys():
+        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+
+    # Build 2 hidden layer DNN with 10, 10 units respectively
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=my_feature_columns,
+        # Two hidden layers of 10 nodes each.
+        hidden_units=[10, 10],
+        # The model must choose between 3 classes.
+        model_dir="./model_dir",
+        n_classes=3)
+
+    classifier.train(input_fn=lambda: iris_data.train_input_fn(train_x,
+                                                               train_y, batch_size),
+                     steps=train_steps)
+
+    # Evaluate the model.
+    eval_result = classifier.evaluate(input_fn=lambda: iris_data.eval_input_fn(test_x,
+                                                                               test_y,
+                                                                               batch_size))
+
+    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+    # Generate predictions from the model
+    expected = ['Setosa', 'Versicolor', 'Virginica']
+    predict_x = {
+        'SepalLength': [5.1, 5.9, 6.9],
+        'SepalWidth': [3.3, 3.0, 3.1],
+        'PetalLength': [1.7, 4.2, 5.4],
+        'PetalWidth': [0.5, 1.5, 2.1],
+    }
+
+    predictions = classifier.predict(
+        input_fn=lambda: iris_data.eval_input_fn(predict_x,
+                                                 labels=None,
+                                                 batch_size=batch_size))
+
+    template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
+
+    for pred_dict, expec in zip(predictions, expected):
+        class_id = pred_dict['class_ids'][0]
+        probability = pred_dict['probabilities'][class_id]
+
+        print(template.format(iris_data.SPECIES[class_id],
+                              100 * probability, expec))
+
+
+if __name__ == '__main__':
+    main()

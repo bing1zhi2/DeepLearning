bc5df1bc9208ed24ed8ca4776903cfef34d18a92
chenhao <bing1zhi2@163.com>
2019-05-10 14:42:54 +0800

    cnn in tf cp cscs data

---
+++ src/data_set_deal/generate_pascalvod_imagesets_txt.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+import os
+import random
+
+g_root_path = "D:/VOCdevkit/VOC2007/"
+g_root_path = "E:/dataset/my_CSCs_data/mylabel_data/pascalvoc_cscs/"
+xmlfilepath = "Annotations"  # 标注文件存放路径
+saveBasePath = "ImageSets/Main/"  # ImageSets信息生成路径
+trainval_percent = 0.7
+train_percent = 0.7
+
+
+
+os.chdir(g_root_path)
+total_xml = os.listdir(xmlfilepath)
+num = len(total_xml)
+xml_list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(xml_list, tv)
+train = random.sample(trainval, tr)
+
+print("train and val size", tv)
+print("train  size", tr)
+ftrainval = open(saveBasePath + "trainval.txt", "w")
+ftest = open(saveBasePath + "test.txt", "w")
+ftrain = open(saveBasePath + "train.txt", "w")
+fval = open(saveBasePath + "val.txt", "w")
+
+for i in xml_list:
+    name = total_xml[i][:-4] + "\n"
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftrain.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftest.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
+
--- src/opencv_learn/img_compress.py
+++ src/opencv_learn/img_compress.py
@@ -1,4 +1,8 @@
+# -*- coding:utf-8 -*-
 import cv2
 
-img = cv2.imread('F:\\work\\catch\\2.jpg')
-cv2.imwrite('F:\\work\\catch\\test_compress.jpg', img, (cv2.IMWRITE_JPEG_QUALITY, 30))
+# img = cv2.imread('F:\\work\\catch\\2.jpg')
+# cv2.imwrite('F:\\work\\catch\\test_compress.jpg', img, (cv2.IMWRITE_JPEG_QUALITY, 30))
+
+img = cv2.imread('F:/work/高铁供电设备故障检测/缺陷\缺陷1.jpg')
+print(img.shape)
